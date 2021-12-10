844b06559cb93a3d88656f8d4a7dde4da2b36b36
Kristina Voit <k.voit.v.thun@web.de>
2021-12-10 16:04:05 +0100

    Finalized cards with image, background-color and hover-effect

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,13 +1,40 @@
+import styled from "styled-components";
 function Card({name, species, image}) {
     
    
 
     return (
-    <ul>
-        <li>{name}</li>
-        <li>{species}</li>
-        <li>{image}</li>
-    </ul>)
+    <section>
+        <Carddesign>
+            <Namedesign>{name}</Namedesign>
+            <div>{species}</div>
+            <Imagedesign src={image}/>
+        </Carddesign>
+    </section>)
 }
    
   export default Card
+
+  const Carddesign = styled.div`
+    border: 2px solid black;
+    border-radius: 20px;
+    box-shadow: 5px 5px 10px black;
+    margin: 2rem;
+    padding-bottom: 1rem;
+    background-color: rgba(250, 128, 114, 0.557);
+
+    &:hover {
+    background-color: green;
+  }
+    `;
+
+
+
+  const Namedesign = styled.div`
+    font-size: 2rem;  
+  `
+
+const Imagedesign = styled.img`
+    border-radius: 10px;
+     
+`
