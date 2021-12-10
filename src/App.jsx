119b3e474cb2ba008fd9cbd5bc105b6ef1aabaa9
Kristina Voit <k.voit.v.thun@web.de>
2021-12-10 13:03:50 +0100

    API fetch worked

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,49 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 import './App.css'
 
 function App() {
+const [characters, setCharacters] = useState([]);
+console.log(characters)
+useEffect(() => {
+  fetch('https://rickandmortyapi.com/api/character')
+  .then((response) => response.json())
+  .then((data) => {
 
-  return (
+    const allCharacters = data.results.slice(0, 10).map((character) => ({
+      name: character.name,
+      species: character.species,
+      image: character.image,
+    }));
+
+    setCharacters(allCharacters)
+
+  });
+}, []);
+
+function addTodo(todo) {
+  setCharacters([...todos, todo]);
+}
+
+
+
+return (
     <div className="App">
-
-      <p>Test</p>
-
+      <h1>Rick and Morty Characters</h1>
+     
+     <div>TEST</div>
+     
+     
+      {/*{characters &&
+        characters.map((characters, index) => (
+          <Card key={index} name={character.name} species={character.species} />
+        ))} */}
     </div>
+      
   )
 }
 
 export default App
+
+
